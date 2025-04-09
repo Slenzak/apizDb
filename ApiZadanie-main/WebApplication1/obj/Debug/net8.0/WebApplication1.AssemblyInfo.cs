020d44ffd4368d6254194a9dac7717ad45a43156
Slenzak <kubolek2007.1997@wp.pl>
2025-04-09 20:41:54 +0200

    s

--- ApiZadanie-main/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ ApiZadanie-main/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e74d38e52a0e0b42dd3080818b6308346081f89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0237039573c1a06473b3222a980f2c8e8c502c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
